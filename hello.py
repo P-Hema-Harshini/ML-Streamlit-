8644ffd4bce30a9f6532fa320405d2bf244d062a
P-Hema-Harshini <140413049+P-Hema-Harshini@users.noreply.github.com>
2024-08-24 11:23:49 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.write("Atlast Done")
+st.title("MY WEB PAGE USING STREAMLIT🥳")
+if st.checkbox("Do you wanna select it...🙂"):
+   st.write("You did it..")
+   st.header("Thinking!!!!")
+   
