386d842159a6293198c60ac5abd5bdfd9b4168de
P-Hema-Harshini <140413049+P-Hema-Harshini@users.noreply.github.com>
2024-08-24 19:35:55 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.write("Atlast Done")
 st.title("MY WEB PAGE USING STREAMLIT🥳")
+st.header("This is a main heading")
 if st.checkbox("Do you wanna select it...🙂"):
    st.write("You did it..")
    st.header("Thinking!!!!")
